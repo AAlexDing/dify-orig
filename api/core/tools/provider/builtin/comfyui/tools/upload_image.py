9ca0da8b63ed4e77989e6ff36cca45c3e5aa116d
Jiatong Ding <dingjiatong@gmail.com>
2024-11-20 17:45:39 +0800

    feat:support multiple image upload

--- api/core/tools/provider/builtin/comfyui/tools/upload_image.py
+++ api/core/tools/provider/builtin/comfyui/tools/upload_image.py
@@ -3,6 +3,7 @@ from typing import Any, Union
 
 from httpx import get
 
+from configs import dify_config
 from core.file.models import File
 from core.tools.entities.common_entities import I18nObject
 from core.tools.entities.tool_entities import ToolInvokeMessage, ToolParameter
@@ -19,33 +20,43 @@ class UploadImageTool(BuiltinTool):
         if not base_url:
             return self.create_text_message("请输入base_url")
         comfyui_api = ComfyUiClient(base_url, token)
-        image_file = tool_parameters.get("image_file", [])
+        image_files = tool_parameters.get("image_file", [])
 
-        if not image_file or not isinstance(image_file, list) or len(image_file) == 0:
+        if not image_files or not isinstance(image_files, list) or len(image_files) == 0:
             return self.create_text_message("未提供有效的图片文件")
         
-        file_info = image_file[0]
-        if not isinstance(file_info, File):
-            return self.create_text_message("无效的文件对象")
+        results = {}
+        for idx, file_info in enumerate(image_files):
+            if not isinstance(file_info, File):
+                return self.create_text_message("无效的文件对象")
+            
+            file_content = self.get_file_content(file_info)
+            if not file_content:
+                return self.create_text_message("无法获取图片文件内容")
+            
+            file_object = io.BytesIO(file_content)
+            file_object.name = file_info.filename or "uploaded_image.png"
+            
+            upload_resp = comfyui_api.upload_image(file_object)
+            if not upload_resp:
+                results[str(idx)] = ""
+                continue
+            
+            subfolder = upload_resp.get("subfolder")
+            name = upload_resp.get("name")
+            if not subfolder:
+                results[str(idx)] = name
+            else:
+                results[str(idx)] = subfolder + '\\' + name
         
-        file_content = self.get_file_content(file_info)
-        if not file_content:
-            return self.create_text_message("无法获取图片文件内容")
-        
-        file_object = io.BytesIO(file_content)
-        file_object.name = file_info.filename or "uploaded_image.png"
-        
-        result = comfyui_api.upload_image(file_object)
-        if result:
-            return self.create_text_message(f"{result}")
-        else:
-            return self.create_text_message("")
+        return self.create_json_message(results)
         
     def get_file_content(self, file: File):
         url = file.generate_url()
         if url:
             if not url.startswith("http"):
-                url = "http://kanju.la:33333" + url
+                base_url = dify_config.FILES_URL.rstrip("/")
+                url = base_url + url
             response = get(url)
             if response.status_code == 200:
                 return response.content
